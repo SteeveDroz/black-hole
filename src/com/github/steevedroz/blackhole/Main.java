be179254a1fb88e6348c39e5cea3351b298d4772
Steeve Droz <steeve.droz@sudest-webdesign.com>
2016-03-25 00:08:40 +0100

    It is possible to select the size of the board

--- src/com/github/steevedroz/blackhole/Main.java
+++ src/com/github/steevedroz/blackhole/Main.java
@@ -1,26 +1,52 @@
 package com.github.steevedroz.blackhole;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+    @Override
+    public void start(Stage primaryStage) {
+	try {
+	    BorderPane root = new BorderPane();
+	    Scene scene = new Scene(root, 400, 400);
+	    scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+	    ComboBox<Integer> sizePicker = new ComboBox<Integer>();
+	    sizePicker.getItems().add(null);
+	    for (int i = 3; i < 15; i++) {
+		if (BlackHole.checkSize(i)) {
+		    sizePicker.getItems().add(i);
 		}
+	    }
+	    sizePicker.addEventHandler(ActionEvent.ACTION, new EventHandler<Event>() {
+		@Override
+		public void handle(Event event) {
+		    try {
+			if (sizePicker.getValue() != null) {
+			    root.setCenter(new BlackHole(sizePicker.getValue()));
+			}
+		    } catch (BadSizeException e) {
+			sizePicker.setValue(null);
+		    }
+		}
+	    });
+
+	    root.setCenter(sizePicker);
+	    primaryStage.setScene(scene);
+	    primaryStage.setTitle("Black Hole");
+	    primaryStage.show();
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    }
+
+    public static void main(String[] args) {
+	launch(args);
+    }
 }
